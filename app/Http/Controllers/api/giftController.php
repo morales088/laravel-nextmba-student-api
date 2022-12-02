2de40a19d944c3bd96e18236d67d80992ba32436
Neil <johnneilmorales@gmail.com>
2022-12-02 20:43:52 +0800

    gift bug fix

--- app/Http/Controllers/api/giftController.php
+++ app/Http/Controllers/api/giftController.php
@@ -28,7 +28,7 @@ class giftController extends Controller
         //                     left join studentcourses sc ON sc.studentId = p.student_id and sc.courseId = c.id
         //                     where pi.status <> 0 and c.id <> 0 and sc.status <> 0 and p.status = 'Paid' and p.student_id = $userId");
 
-        $courses = DB::SELECT("select c.id course_id, c.name course_name, pi.quantity course_qty, p.id payment_id, p.student_id, pi.giftable as unconsumed_course, IF(p.created_at < '$date', true, false) is_giftable
+        $courses = DB::SELECT("select c.id course_id, c.name course_name, pi.quantity course_qty, p.id payment_id, p.student_id, pi.giftable as unconsumed_course, IF(p.created_at > '$date', true, false) is_giftable
                             from payments p
                             left join payment_items pi ON p.id = pi.payment_id
                             left join courses c ON c.id = pi.product_id
