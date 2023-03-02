ffd6a9658ca2ecf77883e7d7cef4490b86663aed
Neil <johnneilmorales@gmail.com>
2023-03-02 18:31:50 +0800

    watch tumbnail

--- app/Http/Controllers/api/streamController.php
+++ app/Http/Controllers/api/streamController.php
@@ -20,6 +20,13 @@ class streamController extends Controller
             'uid' => 'required|string',
         ]);
         
+
+        $response_thumbnail = Http::acceptJson()->withHeaders([
+            'Authorization' => "Bearer $stream_api_key",
+        ])->post($stream_link."/accounts/$stream_account_id/stream/$request->uid", [
+            'thumbnailTimestampPct' => 0.1,
+        ]);
+
         $response = Http::acceptJson()->withHeaders([
             'Authorization' => "Bearer $stream_api_key",
         ])->get($stream_link."/accounts/$stream_account_id/stream/$request->uid", [
@@ -28,15 +35,6 @@ class streamController extends Controller
 
         $cf_response_result = $response->json()['result'];
         // dd($cf_response_result['thumbnailTimestampPct'] == 0, $cf_response_result['thumbnailTimestampPct']);
-        if($cf_response_result['thumbnailTimestampPct'] == 0){
-
-            $response_thumbnail = Http::acceptJson()->withHeaders([
-                'Authorization' => "Bearer $stream_api_key",
-            ])->post($stream_link."/accounts/$stream_account_id/stream/$request->uid", [
-                'thumbnailTimestampPct' => 0.1,
-            ]);
-
-        }
 
         $time = now()->addHours(3);
 
